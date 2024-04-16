52dd640b89d44edb0d9ef28934426ea2419cac1d
C0A23130 <c0a2313028@edu.teu.ac.jp>
2024-04-16 18:17:16 +0900

    課題

--- pygame_image.py
+++ pygame_image.py
@@ -20,19 +20,22 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         key_lst = pg.key.get_pressed()
+        x = -1
+        y = 0
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip([0, -1])
-        if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip([0, +1])
-        if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip([+1, 0])
-        if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip([-1, 0])
-        x = tmr%3200
-        screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img2, [-x+1600, 0])
-        screen.blit(bg_img, [-x+3200, 0])
-        screen.blit(bg_img2, [-x+4800, 0])
+            y = -1
+        elif key_lst[pg.K_DOWN]:
+            y = 1
+        elif key_lst[pg.K_RIGHT]:
+            x = 2
+        elif key_lst[pg.K_LEFT]:
+            x = -1
+        kk_rct.move_ip([x, y])
+        z = tmr%3200
+        screen.blit(bg_img, [-z, 0])
+        screen.blit(bg_img2, [-z+1600, 0])
+        screen.blit(bg_img, [-z+3200, 0])
+        screen.blit(bg_img2, [-z+4800, 0])
         screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1        
